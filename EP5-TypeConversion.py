8a0cbb2ccf4ac2cd8cb144555a18e47124deffe0
peerapattop <peerapatza401@gmail.com>
2023-06-05 23:50:17 +0700

    การแปลงชนิดข้อมูล

---
+++ EP5-TypeConversion.py
@@ -0,0 +1,23 @@
+#(5)การแปลงชนิดข้อมูล
+
+x = 10
+y = 9.14
+z = "20"
+
+
+result1 = x+int(z) # แปลง string to int 
+result2 = str(x)+z # แปลง int to string
+
+result3 = x+float(z) # แปลง string to float
+result4 = str(y)+z # แปลง float to string
+
+print(float(x)) # แปลง int to float
+print(int(y)) # แปลง float to int
+
+# print(result1)
+# print(result2)
+# print(result3)
+# print(result4)
+
+
+
