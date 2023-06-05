77310f7e375e3b1e739a74499a4ea1add59cfb30
peerapattop <peerapatza401@gmail.com>
2023-06-05 20:10:36 +0700

    ฟังก์ชั่นprint,ตัวแปรและชนิดข้อมูล

--- EP1-Print.py
+++ EP1-Print.py
@@ -1,1 +1,13 @@
-print("Hello")
+#string => ข้อความ ตัวอักษร
+#number => integer ,float
+#boolean => true , false
+
+print("Hello") 
+print(0) 
+print(-1)
+print(1+9)
+print(3.99)
+print(True)
+print(False)
+print("peerapat \n") # \n ขึ้นบรรทัดใหม่
+print("peerapat\t"+"tangpanyawai") # \t ช่องว่าง
---
+++ EP3-DataTypeVariable.py
@@ -0,0 +1,10 @@
+# Data Type & Variable
+# ชื่อตัวแปร = ค่าที่เก็บในตัวแปร
+
+x = 10
+y = 3.99
+z = True
+print("ผลลัพธ์ของ x = " +str(x)) #แปลงเป็น string โดยใช้ str
+print(y) 
+print(z)
+print(type(y)) #ใช้ type เพื่อ เช็คชนิดข้อมูล
