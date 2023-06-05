47e4ce21c2ccd902e36c2553c250b3c64cccdb2e
peerapattop <peerapatza401@gmail.com>
2023-06-06 00:09:34 +0700

    ฟังก์ชั่น INPUT

---
+++ EP6-Input.py
@@ -0,0 +1,12 @@
+#(6)ฟังก์ชั่น INPUT
+
+# firstname = input("ชื่อของคุณ : ")
+# lastname = input("นามสกุลของคุณ : ")
+# print("ชื่อของคุณคือ : "+firstname)
+# print("นามสกุลของคุณคือ : "+lastname)
+
+x = input("ตัวเลขตัวที่ 1 : ")
+y = input("ตัวเลขตัวที่ 2 : ")
+
+z=int(x)+int(y)
+print("ผลรวมคือ : "+str(z))
